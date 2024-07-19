e64ef872059dbf91f7440f322b8821a28cffb974
uzair-bmj <uzairbmj@gmail.com>
2024-07-19 20:58:23 +0500

    db connection error resolved

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+import mongoose from "mongoose";
 import { app } from "./app.js";
 import { config } from "dotenv";
-import dbConnection from "./db/index.js";
+import { db_name } from "./constants.js"
 
 config({
     path: "./env"
 });
 
 
-dbConnection()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, (req, res) => {
-            console.log(`Server is Running on Port ${process.env.PORT}`);
-        })
-    })
-    .catch(error => {
-        console.log("database Connection failed", error);
-    })
+try {
+    const connection = await mongoose.connect(`${process.env.MONGODB_URL}`)
+    console.log(`Database Connected !! Host ${connection.connection.host}/${db_name}`);
+} catch (error) {
+    console.log(`Database Connection error ${error}`)
+}
+
+app.listen(process.env.PORT || 8000, (req, res) => {
+    console.log(`Server is Running on Port ${process.env.PORT}`);
+})
